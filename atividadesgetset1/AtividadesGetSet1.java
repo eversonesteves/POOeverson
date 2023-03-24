1cc3391e467d9710c17dfdaa8b7f9d871e196822
eversonesteves <119497435+eversonesteves@users.noreply.github.com>
2023-03-24 09:29:51 -0300

    Update AtividadesGetSet1.java

--- atividadesgetset1/AtividadesGetSet1.java
+++ atividadesgetset1/AtividadesGetSet1.java
@@ -15,21 +15,20 @@ public class AtividadesGetSet1 {
         item1.setDescricao("Bolacha");
         item1.setQuantidadeComprada(6);
         item1.setValorUnitario(2);
+        System.out.println(item1.getDescricao() + " - Valor total: " + item1.valorFatura() + "\n");
 
         Fatura item2 = new Fatura();
         item2.setNumeroItem(456);
         item2.setDescricao("Toddynho");
         item2.setQuantidadeComprada(-2);
         item2.setValorUnitario(15.0);
+        System.out.println(item2.getDescricao() + " - Valor total: " + item2.valorFatura() + "\n");
 
         Fatura item3 = new Fatura();
         item3.setNumeroItem(75632);
         item3.setDescricao("Pão de Queijo");
         item3.setQuantidadeComprada(10);
         item3.setValorUnitario(3.5);
-
-        System.out.println(item1.getDescricao() + " - Valor total: " + item1.valorFatura() + "\n");
-        System.out.println(item2.getDescricao() + " - Valor total: " + item2.valorFatura() + "\n");
         System.out.println(item3.getDescricao() + " - Valor total: " + item3.valorFatura() + "\n");
 
     }
